798184c4f89a47db55abfd01676240e5bb274f0b
Andrew <andrew.george.sinclair@gmail.com>
2015-12-15 09:57:04 -0600

    fixed the input parsing for day 6

--- day-6/src/day_6/algo.clj
+++ day-6/src/day_6/algo.clj
@@ -1,9 +1,14 @@
 (ns day-6.algo)
 
+(def lights
+  ; this blows up GC
+  (for [y (repeat 1000)]
+    (vec (repeat 1000 nil))))
+
 (defn do-algo-1
   ""
-  [word]
-  nil)
+  [commands]
+  )
 
 (defn do-algo-2
   ""
--- day-6/src/day_6/input.clj
+++ day-6/src/day_6/input.clj
@@ -3,26 +3,22 @@
 
 (defn parse-coords
   "e.g., 660,55 through 986,197"
-  [rectangle-part]
-  (let [[_ x1 y1 x2 y2] (re-find #"(\d+).+?(\d+).+?(\d+).+?(\d+)" rectangle-part)]
+  [line]
+  (let [[_ x1 y1 x2 y2] (re-find #".+?(\d+).+?(\d+).+?(\d+).+?(\d+)" line)]
     {:pt1 {:x x1 :y y1} :pt2 {:x x2 :y y2}}))
 
 (defn parse-input
   [line]
-  (let [words (str/split line #" ")]
-    (cond
-      (= (first words) "toggle")
-        #{:command :toggle :rect (parse-coords (next words))}
-      :else
-        (let [rectangle-part (next (next words))
-              rectangle (parse-coords rectangle-part)]
-          (if (= (second words) "on")
-            #{:command :on  :rect rectangle}
-            #{:command :off :rect rectangle})))))
+  (let [words (str/split line #" ")
+        rectangle (parse-coords line)]
+    (if (= (first words) "toggle")
+      {:command :toggle :rect rectangle}
+      (if (= (second words) "on")
+        {:command :on  :rect rectangle}
+        {:command :off :rect rectangle}))))
 
 (defn get-input
   "Returns input from a file"
   [file-name]
-  ; TODO there is an issue with parse-input wants a string, line-seq returns something else
   (with-open [reader (clojure.java.io/reader file-name)]
     (reduce conj '() (map parse-input (line-seq reader)))))
