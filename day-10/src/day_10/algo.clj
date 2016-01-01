728ad25ee6b9c4b76a302b1b18a23c89710bf053
AndrewSinclair <andrew.sinclair@skipthedishes.ca>
2015-12-31 23:30:58 -0600

    Finished day 10. Trick to optimizing was to remove any string concatenation and use integer lists instead

--- day-10/src/day_10/algo.clj
+++ day-10/src/day_10/algo.clj
@@ -1,16 +1,23 @@
 (ns day-10.algo)
 
-(defn algo-fn
+(defn look-and-say
+  [int-list]
+  (let [partitioned (partition-by identity int-list)
+        seq-length  (map #(list (count %) (first %)) partitioned)]
+    (flatten seq-length)))
+
+(defn make-int-list
   [number]
-  (let [partitioned-chars (map #(list (count %) (first %)) (partition-by identity number))]
-    (reduce str (map #(str (first %) (second %)) partitioned-chars))))
+  (map #(- (int %) (int \0)) number))
 
 (defn do-algo-1
   "Counts the digits after playing the look-and-say game 40 times"
   [number]
-  (count (nth (iterate algo-fn number) 40)))
+  (let [int-list (make-int-list number)]
+    (count (nth (iterate look-and-say int-list) 40))))
 
 (defn do-algo-2
-  "Counts the digits after playing the look-and-say game 50 times"
-  [strings]
-  (count (nth (iterate algo-fn number) 50)))
+  "Counts the digits after playing the look-and-say game 50 times. Takes about 20 seconds on modern hardware."
+  [number]
+  (let [int-list (make-int-list number)]
+    (count (nth (iterate look-and-say int-list) 50))))
