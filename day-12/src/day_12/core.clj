0182e7a2adee6854d0aadb3b375e316df6bc2653
Andrew <andrew.george.sinclair@gmail.com>
2016-01-05 09:23:06 -0600

    tidy some whitespace

--- day-12/src/day_12/core.clj
+++ day-12/src/day_12/core.clj
@@ -1,9 +1,9 @@
 (ns day-12.core
   (:use [day-12.input]
-  		[day-12.algo])
+        [day-12.algo])
   (:gen-class))
 
-(def file-name "resource/input.txt")
+(def file-name "resources/input.txt")
 
 (defn -main
   "Advent of Code - Day 12 - Json File"
