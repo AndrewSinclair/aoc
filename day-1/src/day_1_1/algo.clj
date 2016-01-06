8c49c2072b70882a72a66001ad49229bea56f80c
AndrewSinclair <andrew.sinclair@skipthedishes.ca>
2016-01-05 22:44:50 -0600

    Cleaned up day 1

--- day-1/src/day_1_1/algo.clj
+++ day-1/src/day_1_1/algo.clj
@@ -1,15 +1,9 @@
-(ns day-1-1.algo
-  (:use
-    [clojure.core.match :refer [match]]))
-
-(defn get-input [fileName]
-  (slurp fileName))
+(ns day-1-1.algo)
 
 (defn do-algo-1
   "Counts the parens and figures out what floor santa is on"
   [input]
-
-  (reduce + 
+  (reduce +
     (map
       #(if (= % \() 1 -1)
       input)))
@@ -17,13 +11,9 @@
 (defn do-algo-2
   "Counts the parens and finds the index when santa enters the basement"
   [input]
-
   (loop [[paren & rest] input
          index 0
          accum 0]
     (if-not (or (neg? accum) (nil? paren))
       (recur rest (inc index) (+ accum (if (= paren \() 1 -1)))
-      index
-    )
-  )
-)
+      index)))
