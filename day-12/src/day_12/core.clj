4abb92e62026e044ff029c03ff50827a62b743ce
AndrewSinclair <andrew.sinclair@skipthedishes.ca>
2016-01-05 22:39:40 -0600

    Finished day 12

--- day-12/src/day_12/core.clj
+++ day-12/src/day_12/core.clj
@@ -6,9 +6,8 @@
 (def file-name "resources/input.txt")
 
 (defn -main
-  "Advent of Code - Day 12 - Json File"
+  "Advent of Code - Day 12 - Json file with numbers and filtering out 'red' attributes."
   [& args]
   (do
-    ;(println (do-algo-1 (get-input file-name)))
-    (println (do-algo-2 (get-input file-name)))
-    ))
+    (println (do-algo-1 (get-input file-name)))
+    (println (do-algo-2 (get-input file-name)))))
