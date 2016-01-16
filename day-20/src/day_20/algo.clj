1058ae01fab9c7de13cc70e1faac74e0aa81105a
Andrew Sinclair <andrew.george.sinclair@gmail.com>
2016-01-16 03:28:01 -0600

    Re-worked on Day 20 part 1 so that is uses dynamic programming, and properties of prime numbers and divisors, to speed up the algorithm

---
+++ day-20/src/day_20/algo.clj
@@ -0,0 +1,76 @@
+(ns day-20.algo
+  (:require [clojure.math.numeric-tower :as math]))
+
+(declare r-sum-divisors)
+
+;https://crossclj.info/fun/clojure.contrib.lazy-seqs/primes.html
+(def primes
+  (concat 
+   [2 3 5 7]
+   (lazy-seq
+    (let [primes-from
+    (fn primes-from [n [f & r]]
+      (if (some #(zero? (rem n %))
+          (take-while #(<= (* % %) n) primes))
+        (recur (+ n f) r)
+        (lazy-seq (cons n (primes-from (+ n f) r)))))
+    wheel (cycle [2 4 2 4 6 2 6 4 2 4 6 6 2 6  4  2
+      6 4 6 8 4 2 4 2 4 8 6 4 6 2  4  6
+      2 6 6 4 2 4 6 2 6 4 2 4 2 10 2 10])]
+      (primes-from 11 wheel)))))
+
+(defn next-prime [n]
+  (->>  primes
+        (filter (partial < n))
+        first))
+
+(defn closed-form-sum
+  [prime exponent]
+  (quot
+    (dec (math/expt prime (inc exponent)))
+    (dec prime)))
+
+(defn maximize-exponent
+  [n p]
+  (loop [k 0]
+    (let [p-to-k (math/expt p (inc k))]
+      (cond
+        (= n p-to-k)
+          [p (inc k)]
+        (not (= 0 (rem n p-to-k)))
+          [p k]
+        :else
+          (recur (inc k))))))
+
+(def m-sum-divisors (memoize
+  (fn [n]
+    (r-sum-divisors n))))
+
+(defn r-sum-divisors
+  ([n]
+    (if (.isProbablePrime (BigInteger/valueOf n) 5)
+      (closed-form-sum n 1)
+      (r-sum-divisors n 2)))
+  ([n p]
+    (if (= n p)
+      (closed-form-sum n 1)
+      (if (= 0 (rem n p))
+        (let [[prime exp] (maximize-exponent n p)
+              factor      (math/expt prime exp)]
+          (if (= factor n)
+            (closed-form-sum prime exp)
+            (* (closed-form-sum prime exp)
+               (m-sum-divisors (quot n factor)))))
+        (recur n (next-prime p))))))
+
+(defn do-algo-1
+  [puzzle-input]
+  (let [total-elf-visits (quot puzzle-input 10)]
+    (->>  (range)
+          (drop 2)
+          (filter #(->> % m-sum-divisors (<= total-elf-visits)))
+          first)))
+
+(defn do-algo-2
+  [input]
+  nil)
--- day-20/src/day_20/core.clj
+++ day-20/src/day_20/core.clj
@@ -1,45 +1,13 @@
 (ns day-20.core
-  (:require [clojure.math.numeric-tower :as math])
+  (:use [day-20.algo])
   (:gen-class))
 
-(defn divisors
-  [n]
-  (filter (comp zero? (partial rem n)) (range 1 (inc (quot n 2)))))
+(def input 33100000)
 
-(comment
-(def m-factors (memoize
-  (fn [n k]
-   (if (= 0 (rem n k))
-     k
-     (recur n (inc k))))))
-
-(defn factors
-  "Return a list of factors of N."
-  [n k acc]
-  (if (= n k)
-    acc
-    (let [k2 (m-factors n k)]
-      (recur n (inc k) (cons k2 acc)))))
-
-(def factors-of-N (memoize (fn [n] (factors n 2 ()))))
-)
-
-(comment
-"I think this is actually not strictly necessary and is just more computations overall, so don't use it"
-(defn compress-to-divisors
-  "run length encoding"
-  [elements]
-  (->> (partition-by identity elements) (map (juxt first count))))
-) ; end comment
-  
-(defn sum-of-divisors
-  ""
-  [divisors]
-  (reduce + divisors))
-  
 (defn -main
   "Day 20 - Advent of Code - Elves are delivering presents and you have to factor integers."
   [& args]
-  ;(let [puzzle-input 3310000] ; Divided by 10
-  (let [puzzle-input (quot (first args) 10)]
-    (first (filter #(->> % divisors (cons %) sum-of-divisors (<= puzzle-input)) (drop 2 (range))))))
+  (do
+    (println (do-algo-1 input))
+    (println (do-algo-2 input))))
+
