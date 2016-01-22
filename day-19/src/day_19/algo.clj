3591f3993824b6a01efca200f1c5658c924a03d6
Andrew <andrew.george.sinclair@gmail.com>
2016-01-22 13:28:13 -0600

    parsed input properly for day-19

---
+++ day-19/src/day_19/algo.clj
@@ -0,0 +1,9 @@
+(ns day-19.algo)
+
+(defn do-algo-1
+  [chem-transitions start-molecule]
+  nil)
+
+(defn do-algo-2
+  [chem-transitions start-molecule]
+  nil)
--- day-19/src/day_19/input.clj
+++ day-19/src/day_19/input.clj
@@ -1,15 +1,17 @@
 (ns day-19.input)
 
-(defn to-chemical-map
-  [text chemical-map]
-    (let [[chemical-symbol replacement] (re-find #".+?(\d+).+?(\d+).+?(\d+).+?(\d+)" line)]
-      (assoc chemical-map chemical-symbol replacement))
-  )
+(defn to-chemical-multi-map
+  [chemical-map text]
+    (let [[_ chemical-symbol replacement] (re-find #"(\w+)\s+=>\s+(\w+)" text)]
+      (let [multi-part (get chemical-map chemical-symbol)]
+        (if multi-part
+          (assoc chemical-map chemical-symbol (conj multi-part replacement))
+          (assoc chemical-map chemical-symbol [replacement])))))
 
 (defn get-input
   [file-name]
   (with-open [reader (clojure.java.io/reader file-name)]
-    (let [input-lines   (line-seq reader)
-          chemicals     (reduce to-chemical-map {} (take-while #(complement (= "\n" %)) input-lines))
-          initial-input (last input-lines)]
-      [chemicals initial-input])))
+    (let [input-lines    (line-seq reader)
+          chemicals      (reduce to-chemical-multi-map {} (take-while #(not (= "" %)) input-lines))
+          start-molecule (last input-lines)]
+      [chemicals start-molecule])))
