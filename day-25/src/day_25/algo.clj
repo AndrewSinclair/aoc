21fd2605c435818c68f0bcf8e7545ac26d766b6e
Andrew <andrew.george.sinclair@gmail.com>
2016-02-17 13:01:44 -0600

    Finished day 25

--- day-25/src/day_25/algo.clj
+++ day-25/src/day_25/algo.clj
@@ -1,10 +1,19 @@
 (ns day-25.algo)
 
+(defn map-grid-cell-num
+  [row col]
+  (+ (/ (* (dec (+ row col)) (- (+ row col) 2)) 2) col))
+
+(defn iterator
+  [x]
+  (rem (* 252533 x) 33554393))
+
 (defn do-algo-1
-  [input]
-  nil)
+  [row col]
+  (let [grid-cell-num (map-grid-cell-num row col)]
+    (nth (iterate iterator 20151125) (dec grid-cell-num))))
 
 (defn do-algo-2
-  [input]
+  [row col]
   nil)
 
